1087a1e48e0432801dc94a7884bb947f369550c0
LiBe <libe4@free.fr>
2020-04-12 22:25:52 +0200

    improving the cars behavior

--- src/main/java/com/egencia/puzzle/crossing/traffic/CurrentTraffic.java
+++ src/main/java/com/egencia/puzzle/crossing/traffic/CurrentTraffic.java
@@ -9,28 +9,29 @@ import java.util.concurrent.atomic.AtomicReference;
 class CurrentTraffic {
     private static final AtomicReference<Map<UUID, Car>> TRAFFIC = new AtomicReference<>(new HashMap<>());
 
-    synchronized static Traffic getTraffic (){
+    synchronized static Traffic getTraffic() {
         synchronized (TRAFFIC) {
             return new Traffic(new ArrayList<>(TRAFFIC.get().values()));
         }
     }
 
-    public static synchronized void newCar (Car car) {
+    public static synchronized void newCar(Car car) {
         synchronized (TRAFFIC) {
             TRAFFIC.get().put(car.getCarId(), car);
         }
     }
 
-    public static synchronized void moveCar (MoveCarData moveCarData){
-            synchronized (TRAFFIC) {
+    public static synchronized void moveCar(MoveCarData moveCarData) {
+        synchronized (TRAFFIC) {
+            if (TRAFFIC.get().get(moveCarData.getCarId()) != null)
                 TRAFFIC.get().put(moveCarData.getCarId(),
                         TRAFFIC.get().get(moveCarData.getCarId()).with(moveCarData.getNewSituation()));
-            }
+        }
     }
 
-    public static synchronized void removeCar(RemoveCarData removeCarData){
-            synchronized (TRAFFIC) {
-                TRAFFIC.get().remove(removeCarData.getCarId());
-            }
+    public static synchronized void removeCar(RemoveCarData removeCarData) {
+        synchronized (TRAFFIC) {
+            TRAFFIC.get().remove(removeCarData.getCarId());
         }
+    }
 }
